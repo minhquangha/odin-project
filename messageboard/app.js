a282d06427db57c062997bd739273aa20aab7c90
MinhQuang <minhquanghmqno1@gmail.com>
2025-08-23 16:59:46 +0700

    add view for root url

--- messageboard/app.js
+++ messageboard/app.js
@@ -1,11 +1,30 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const path = require( 'path');
 require('dotenv').config();
-const PORT = process.env.PORT
-app.get("/",(req,res)=>{
-    res.send("Hello");
-})
-console.log(PORT);
-app.listen(PORT,(req,res)=>{
-    console.log("my first app");
-})
+const PORT = process.env.PORT;
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+const messages = [
+    {
+        text: 'Hi there!',
+        user: 'Amando',
+        added: new Date()
+    },
+    {
+        text: 'Hello World',
+        user: 'Charles',
+        added: new Date()
+    }
+];
+app.get('/', (req, res) => {
+    res.render("index", {title: "Mini Messageboard",messages: messages});
+});
+
+app.get('/new', (req, res) => {
+    res.render('newform',)
+});
+app.listen(PORT, (req, res) => {
+    console.log('my first app');
+});
