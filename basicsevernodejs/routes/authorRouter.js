9f54ac4f2ba393ab4e1977c0ce2b385f51d0eaf7
MinhQuang <minhquanghmqno1@gmail.com>
2025-08-23 10:36:23 +0700

    init view using ejs

--- basicsevernodejs/routes/authorRouter.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require("express");
-const authorRouter = Router();
-authorRouter.get("/",(req,res)=>{res.send("All author")});
-authorRouter.get( "/:authorId",(req,res)=>{
-    const {authorId}= req.params;
-    res.send(`Author ID: ${authorId}`);
-});
-
-module.exports = authorRouter;
--- basicsevernodejs/server.js
+++ basicsevernodejs/server.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const authorRouter  = require('./routes/authorRouter');
-app.use(express.static(path.join(__dirname,'static')));
+const assetsPath=path.join(__dirname,"public");
+app.use(express.static(assetsPath));
 
-app.use("/authors",authorRouter);
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'static/index.html'));
+app.set("views" , path.join(__dirname,"views"));
+app.set("view engine" , "ejs" );
+const users = ["Rose", "Cake", "Biff"];
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+app.get("/",(req,res)=>{
+    res.render("index",{links:links, users: users});
 })
 
-app.get('/about',(req,res)=>{res.sendFile(path.join(__dirname,'static/about.html'))});
+app.get("/about",(req,res)=>{
+    res.render("about",{links:links});
+})
+
+
+
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
